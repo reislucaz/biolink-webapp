39f80311ae7a6bd8178ed326ca3c9927b75efcf7
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2024-12-07 18:05:32 -0300

    feat: ✨ add buttons sections

---
+++ src/components/register-steps/buttons-sections.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRegisterContext } from "@/app/contexts/registerContext";
+import { Button } from "../ui/button";
+import { StepsIndicator } from "./steps-indicator";
+import { ReactNode } from "react";
+
+function BackwardsButton(){
+  const { handleChangeStepBackwards } = useRegisterContext()
+  return <Button onClick={handleChangeStepBackwards} size='xl'>Anterior</Button>
+}
+
+function FowardButton(){
+  const { handleChangeStepFoward } = useRegisterContext()
+  return <Button onClick={handleChangeStepFoward} size='xl'>Próximo</Button>
+}
+
+function Root({children}: {children: ReactNode}) {
+  return <div className="flex justify-between items-center">
+    {children}
+  </div>
+}
+
+export const ButtonsSection = {
+  Root,
+  BackwardsButton,
+  FowardButton,
+  StepsIndicator
+}
